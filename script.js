0030c4f151084bf7c0f041295ed119edd6e905f0
Lay Len Ching <lching@andrew.cmu.edu>
2024-11-25 18:11:36 -0500

    updates in econ and description

---
+++ script.js
@@ -0,0 +1,7 @@
+// Select all links on the page
+document.querySelectorAll('a[href]').forEach(link => {
+    // Open external links in a new tab
+    if (link.href.startsWith('http')) {
+        link.setAttribute('target', '_blank');
+    }
+});
